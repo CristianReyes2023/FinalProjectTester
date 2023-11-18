dda2ff156c3ac08f99e64dcd23fea550aa48c1d8
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-11-18 11:26:46 -0500

    feat: :sparkles: Consult 6 GetClientsByCountry

---
+++ BackEnd/Api/Dto/ClientsByCountryDto.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dto;
+public class ClientsByCountryDto
+{
+    public string NameClient { get; set; }
+    public string NameCountry { get; set; }
+}
